22220caa66cd0b9ed0717933ce0e9b84509c5642
urmil22 <urmilbhavsar8@gmail.com>
2022-07-28 11:57:30 +0530

    remove gitignore files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { useSelector, useDispatch } from 'react-redux';
-
+import { actions } from './store/index';
 
 function App() {
-  const counter = useSelector((state) => state.counter);
+  const counter = useSelector((state) => state.counter); //help us to get counter data from index.js store (state= {counter: 0}, action)
   const dispatch = useDispatch();
   const increment = () => {
-    dispatch({type: 'INC'})
+    dispatch(actions.increment())
    }
 
   const decrement = () => { 
-    dispatch({type: 'DEC'})
+    dispatch(actions.decrement())
   }
   const addBy = () => {
-    dispatch({type:'ADD', payload: 10})
+    dispatch(actions.addBy(10));
   }
   return (
     <div >
